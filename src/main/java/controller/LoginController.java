6c1a3260a496a8c4b0db2c581a2bc0908bb36aa5
soker90 <edue17@gmail.com>
2017-10-23 14:47:08 +0200

    Configurada base

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,40 @@
+package controller;
+
+
+import java.io.IOException;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Usuario y/o contraseña inválido");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "Has cerrado sesión correctamente");
+        }
+        model.setViewName("login");
+
+        return model;
+
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/user/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page session="true"%>
+<html>
+    <body>
+        <h1 id="title">Title : ${title}</h1>
+        <h1>Message : ${message}</h1>
+
+    </body>
+</html>
