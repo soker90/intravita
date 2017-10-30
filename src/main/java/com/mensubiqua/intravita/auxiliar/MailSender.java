f952920bc8b8218c57fb3d397110bbe8401a83b8
Jmsl93 <JoseMaria.Simon@alu.uclm.es>
2017-10-30 13:14:32 +0100

    solución error queda el javamail

--- src/main/java/com/mensubiqua/intravita/auxiliar/MailSender.java
+++ src/main/java/com/mensubiqua/intravita/auxiliar/MailSender.java
@@ -9,17 +9,21 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class MailSender {
-	private static String userName = "intravita2017@gmail.com";
-	private static String password = "ulises2017";
+	private static String userName = "designesi.uvly@gmail.com";
+	private static String password = "superpasswordsegura";
 
 	private static Session getSession() throws SQLException {
 		Properties props = new Properties();
-		props.setProperty("mail.smtp.host", "smtp.gmail.com");
-		props.setProperty("mail.smtp.starttls.enable", "true");
-		props.setProperty("mail.smtp.port", "465");
-		props.setProperty("mail.smtp.user", userName);
-		props.setProperty("mail.smtp.auth", "true");
-		props.setProperty("mail.smtp.ssl.trust", "*");
+		props.put("mail.smtp.user", userName);
+	    props.put("mail.smtp.host", "smtp.gmail.com");
+	    props.put("mail.smtp.port", 465);
+	    props.put("mail.smtp.starttls.enable","true");
+	    props.put("mail.smtp.debug", "true");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.socketFactory.port", 465);
+	    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+	    props.put("mail.smtp.socketFactory.fallback", "false");
+
 
 		Session session = Session.getDefaultInstance(props);
 		return session;
