4694f071b78848775891bb62f9225378137352e5
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 20:31:17 +0100

    Primera ejecucion test publicaciones

--- src/test/java/cucumberJava/publicacionesFeatures.feature
+++ src/test/java/cucumberJava/publicacionesFeatures.feature
@@ -3,3 +3,11 @@ Feature: publicacionesFeature
 
 En este test se va a desarrollar toda la parte correspondiente al gestion de publicaciones, nueva publicacion
 editar una publicacion, cambiar visibilidad y eliminar
+
+@Scenario1
+Scenario: crear una nueva publicacion
+	Given Un usuario 
+	And   escribe publicacion
+	When inserta publicacion
+	Then publicacion creada
+	
---
+++ src/test/java/cucumberJava/publicacionesFeatures.java
@@ -0,0 +1,45 @@
+package cucumberJava;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+
+//Cucumber imports
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.runtime.*;
+
+import cucumber.api.PendingException;
+
+public class publicacionesFeatures {
+	
+	@Given("^Un usuario$")
+	public void un_usuario() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^escribe publicacion$")
+	public void escribe_publicacion() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^inserta publicacion$")
+	public void inserta_publicacion() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^publicacion creada$")
+	public void publicacion_creada() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
