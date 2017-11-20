90e23a53d3d76be608dc7075728d4fd9f30d2c61
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 22:25:36 +0100

    Creado archivo amistades Features

---
+++ src/test/java/cucumberJava/amistadesFeatures.java
@@ -0,0 +1,19 @@
+package cucumberJava;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+
+//Cucumber imports
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.runtime.*;
+
+import cucumber.api.PendingException;
+
+public class amistadesFeatures {
+
+}
