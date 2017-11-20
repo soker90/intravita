f4956c5d13ef20f6853eb69cadfeebbe2b270634
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 13:06:52 +0100

    Pom nuevo

--- src/test/java/cucumberJava/runTest.java
+++ src/test/java/cucumberJava/runTest.java
@@ -2,9 +2,11 @@ package cucumberJava;
 
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+
 
 public class runTest {
 
