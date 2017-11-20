cd5897951351b92bc5cc702932b7e9dd348d76cc
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 20:50:10 +0100

    Nuevos escenarios en publicaciones y primer test

--- src/test/java/cucumberJava/publicacionesFeatures.feature
+++ src/test/java/cucumberJava/publicacionesFeatures.feature
@@ -2,7 +2,7 @@
 Feature: publicacionesFeature
 
 En este test se va a desarrollar toda la parte correspondiente al gestion de publicaciones, nueva publicacion
-editar una publicacion, cambiar visibilidad y eliminar
+editar una publicacion, eliminar una publicacion ya creada y eliminar todas las publicaciones de un usuario eliminado
 
 @Scenario1
 Scenario: crear una nueva publicacion
@@ -11,3 +11,24 @@ Scenario: crear una nueva publicacion
 	When inserta publicacion
 	Then publicacion creada
 	
+@Scenario2
+Scenario: borrar una publicacion
+	Given Un usuario 
+	And   publicacion creada
+	When borra publicacion
+	Then publicacion no existe en bd
+	
+@Scenario3
+Scenario: editar una publicacion
+	Given Un usuario 
+	And   publicacion creada
+	When edita publicacion
+	Then publicacion modificada
+	
+
+@Scenario4
+Scenario: eliminar publicaciones de un usuario eliminado
+	Given Un usuario "pepe"
+	And publicaciones creadas
+	When Borra usuario "pepe"
+	Then no hay publicaciones de usuario "pepe"
--- src/test/java/cucumberJava/publicacionesFeatures.java
+++ src/test/java/cucumberJava/publicacionesFeatures.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import com.mensubiqua.intravita.dao.UserDAOImpl;
+import com.mensubiqua.intravita.model.User;
+import com.mensubiqua.intravita.model.Publicacion;
+import com.mensubiqua.intravita.dao.PublicacionDAOImpl;
 
 //Cucumber imports
 
@@ -16,7 +20,19 @@ import cucumber.runtime.*;
 
 import cucumber.api.PendingException;
 
+
+
 public class publicacionesFeatures {
+	
+	private User usuario;
+	private UserDAOImpl dao = new UserDAOImpl();
+	private PublicacionDAOImpl daoP = new PublicacionDAOImpl();
+	private Publicacion p = new Publicacion(); 
+	
+	private String nombre = "Miguel";
+	private String apellido = "Ampuero";
+	private String email = "user@email.com";
+	private String pass = "password";
 	
 	@Given("^Un usuario$")
 	public void un_usuario() throws Throwable {
@@ -41,5 +57,54 @@ public class publicacionesFeatures {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
+	
+	@When("^borra publicacion$")
+	public void borra_publicacion() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^publicacion no existe en bd$")
+	public void publicacion_no_existe_en_bd() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^edita publicacion$")
+	public void edita_publicacion() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^publicacion modificada$")
+	public void publicacion_modificada() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Un usuario \"([^\"]*)\"$")
+	public void un_usuario(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^publicaciones creadas$")
+	public void publicaciones_creadas() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Borra usuario \"([^\"]*)\"$")
+	public void borra_usuario(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^no hay publicaciones de usuario \"([^\"]*)\"$")
+	public void no_hay_publicaciones_de_usuario(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
 
 }
