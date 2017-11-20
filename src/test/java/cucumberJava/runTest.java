9c763c790df8487d2ac287f32b0e9f059aeac2cc
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 13:10:22 +0100

    Error POM eliminado

--- src/test/java/cucumberJava/runTest.java
+++ src/test/java/cucumberJava/runTest.java
@@ -1,8 +1,8 @@
 package cucumberJava;
 
-import org.junit.runner.RunWith;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
