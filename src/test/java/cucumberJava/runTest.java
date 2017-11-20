87379dd4dd8957bded96f037c6ccde3369334c61
miguelampu <Miguel.Ampuero@alu.uclm.es>
2017-11-20 13:24:45 +0100

    Path actualizado en runTest

--- src/test/java/cucumberJava/runTest.java
+++ src/test/java/cucumberJava/runTest.java
@@ -5,7 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(
+		plugin = {"pretty", "html:target/cucumber"},
+		features = "src/test/java/cucumberJava")
 
 
 public class runTest {
